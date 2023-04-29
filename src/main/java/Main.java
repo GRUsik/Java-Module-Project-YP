cd0f88cd3bf669b5d34ecf0c0dfa523793917c97
GRUsik <s.u-2000@yandex.ru>
2023-04-29 23:08:44 +0700

    Надеюсь сейчас правильно

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         System.out.println("Введите количество гостей: ");
         int people = 0;
         double shet = 0.00;
